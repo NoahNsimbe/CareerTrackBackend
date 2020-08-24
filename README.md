889cdd5d289fc0da3c598f4be54952cca4b0c4ca
Noah Nsimbe <nsimbenoah@gmail.com>
2020-08-24 17:54:12 +0300

    some cleanup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# CareerTrackBackend
+[![Build Status](https://travis-ci.com/NoahNsimbe/CareerTrackBackend.svg?branch=master)](https://travis-ci.com/NoahNsimbe/CareerTrackBackend)
+# CareerTrack Backend
--- career_track/urls.py
+++ career_track/urls.py
@@ -27,9 +27,9 @@ router.register('get_course', CourseRecommendationViewSet, basename="get_course"
 urlpatterns = [
 
     path('api/admin/', admin.site.urls),
-    path('admin/login/', auth_views.LoginView.as_view(), name='admin_login'),
-    path('admin/password_reset/', auth_views.PasswordResetView.as_view(), name='admin_password_reset'),
-    path('admin/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('api/admin/login/', auth_views.LoginView.as_view(), name='admin_login'),
+    path('api/admin/password_reset/', auth_views.PasswordResetView.as_view(), name='admin_password_reset'),
+    path('api/admin/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
